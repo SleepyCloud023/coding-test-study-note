0dbd93957a3a9a7592086ce19315bc947e178b1c
SleepyCloud023 <gnflwhf352@gmail.com>
2022-03-13 17:34:14 +0900

    solved: 종이 접기

--- Greedy/boj_1802/boj_1802.py
+++ Greedy/boj_1802/boj_1802.py
@@ -1,1 +1,29 @@
 # https://www.acmicpc.net/problem/1802
+# 종이 접기
+
+import sys
+input = sys.stdin.readline
+
+
+def check_yes(input_string: str) -> bool:
+    if len(input_string) == 1:
+        return True
+    idx_center = len(input_string) // 2
+    for i in range(1, idx_center+1):
+        left = input_string[idx_center - i]
+        right = input_string[idx_center + i]
+        if left == right:
+            return False
+
+    return check_yes(input_string[:idx_center])
+
+
+if __name__ == '__main__':
+    N = int(input())
+    string_list = []
+    for _ in range(N):
+        input_string = input().strip()
+        string_list.append(input_string)
+
+    result = ['YES' if check_yes(x) else 'NO' for x in string_list]
+    print(*result, sep='\n')
