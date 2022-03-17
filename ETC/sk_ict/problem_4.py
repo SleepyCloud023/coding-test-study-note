9d5b9624b48458d4765f5272ac23bd1d899c6422
SleepyCloud023 <gnflwhf352@gmail.com>
2022-03-18 01:14:21 +0900

    solved: length over 2 update

--- ETC/sk_ict/problem_4.py
+++ ETC/sk_ict/problem_4.py
@@ -1,29 +1,10 @@
 import sys
-from collections import deque
-
 input = sys.stdin.readline
 
 
 def solution(N: int, graph: list, start: int):
-    dist = [0] * N
-
-    q = deque([start])
-    dist[start] = 1
-
-    while(q):
-        cur_node = q.popleft()
-
-        for next_node in graph[cur_node]:
-            if dist[next_node] == 0:
-                dist[next_node] = dist[cur_node] + 1
-                q.append(next_node)
-
-    count = 0
-    for path_length in dist:
-        if path_length > 2:
-            count += 1
-
-    return count
+    length_below_2 = 1 + len(graph[start])
+    return N - length_below_2
 
 
 if __name__ == '__main__':
@@ -37,7 +18,6 @@ if __name__ == '__main__':
     count = [solution(N, graph, node) for node in range(N)]
     result = sum(count)
     print(result)
-
 
 my_test = '''
 5 4
