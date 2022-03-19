cd60e887d54cf5c781b469d267f951f8ec286986
SleepyCloud023 <gnflwhf352@gmail.com>
2022-03-20 04:35:09 +0900

    sovled: 피보나치 함수

--- DP/boj_1003/boj_1003.py
+++ DP/boj_1003/boj_1003.py
@@ -1,4 +1,36 @@
 # https://www.acmicpc.net/problem/1003
 # title: 피보나치 함수
 # start: 2022-03-20 04:18:04 AM
-# end:   2022-03-20 04:18:04 AM
+# end:   2022-03-20 04:32:04 AM
+
+import sys
+input = sys.stdin.readline
+
+
+def fibo(n):
+    first = [1, 0]
+    second = [0, 1]
+    if n == 0:
+        return first
+    elif n == 1:
+        return second
+
+    n -= 1
+    while(n > 0):
+        next = [first[x] + second[x] for x in range(2)]
+        first = second
+        second = next
+        n -= 1
+
+    return next
+
+
+if __name__ == '__main__':
+    T = int(input())
+    result = []
+    for _ in range(T):
+        n = int(input())
+        result.append(fibo(n))
+
+    for x in result:
+        print(*x)
