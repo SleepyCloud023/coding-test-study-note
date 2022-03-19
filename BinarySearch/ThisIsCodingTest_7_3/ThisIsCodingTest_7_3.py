a0afb3e30dffb8c53a81b5f298844d1afc4bfaf7
SleepyCloud023 <gnflwhf352@gmail.com>
2022-03-19 22:58:07 +0900

    solved: 떡볶이 떡 만들기 update

--- BinarySearch/ThisIsCodingTest_7_3/ThisIsCodingTest_7_3.py
+++ BinarySearch/ThisIsCodingTest_7_3/ThisIsCodingTest_7_3.py
@@ -1,59 +1,37 @@
 # BinarySearch/7_3
 # title: 떡볶이 떡 만들기
-# start: 2022-03-19 09:08:50 PM
-# end:   2022-03-19 10:35:50 PM
+# start: 2022-03-19 10:42:57 PM
+# end:   2022-03-19 10:57:57 PM
 
-from bisect import bisect_right
 import sys
 input = sys.stdin.readline
 
 
-def count_choco(sum_choco, H, idx_H):
-    if idx_H == 0:
-        return 0
-    return sum_choco[idx_H-1] - idx_H * H
-
-
-def find(goal: int, choco_list: list, sum_choco: list, start, end):
-    if start > end:
-        return -1
-
-    mid = (start + end) // 2
-    mid_idx = len(choco_list) - bisect_right(choco_list, mid)
-
-    choco_mid = count_choco(sum_choco, mid, mid_idx)
-
-    if choco_mid >= goal:
-        return mid
-    else:
-        return find(goal, choco_list, sum_choco, start, mid - 1)
-
-
-def get_max_H(goal: int, num_choco: int, choco_list: list):
-    choco_list.sort()
-    sum_choco = [0] * (num_choco)
-    sum_choco[0] = choco_list[-1]
-
-    # sum_choco[k] => 뒤에서 k번째까지의 초코의 누적합
-    for i in range(1, num_choco):
-        sum_choco[i] = sum_choco[i-1] + choco_list[-i-1]
-
-    best_choco_H = 0
+def get_min_H(goal: int, num_choco: int, choco_list):
+    result = 0
     start = 0
-    end = choco_list[-1]
-    while(True):
-        next_choco_H = find(goal, choco_list, sum_choco, start, end)
-        if next_choco_H == -1:
-            break
-        else:
-            best_choco_H = next_choco_H
-            start = best_choco_H + 1
+    end = max(choco_list)
 
-    return best_choco_H
+    while (start <= end):
+        sum_choco = 0
+        mid = (start + end) // 2
+
+        for choco in choco_list:
+            if choco > mid:
+                sum_choco += choco - mid
+
+        if sum_choco >= goal:
+            result = mid
+            start = mid + 1
+
+        else:
+            end = mid - 1
+
+    return result
 
 
 if __name__ == '__main__':
     num_choco, goal = map(int, input().split())
     choco_list = list(map(int, input().split()))
-    max_H = get_max_H(goal, num_choco, choco_list)
-    print(max_H)
+    result = get_min_H(goal, num_choco, choco_list)
+    print(result)
