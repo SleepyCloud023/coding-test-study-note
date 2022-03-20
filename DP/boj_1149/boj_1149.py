3483e7e9acc0431195965174697b27bf7d34212a
SleepyCloud023 <gnflwhf352@gmail.com>
2022-03-20 18:25:51 +0900

    solved: 01타일

---
+++ DP/boj_1149/boj_1149.py
@@ -0,0 +1,4 @@
+# https://www.acmicpc.net/problem/1149
+# title: RGB거리
+# start: 2022-03-20 06:25:10 PM
+# end:   2022-03-20 06:25:10 PM
--- DP/boj_1904/boj_1904.py
+++ DP/boj_1904/boj_1904.py
@@ -1,4 +1,18 @@
 # https://www.acmicpc.net/problem/1904
 # title: 01타일
 # start: 2022-03-20 06:05:56 AM
-# end:   2022-03-20 06:05:56 AM
+# end:   2022-03-20 06:38:56 AM
+
+def fibo(N):
+    dp = [0] * (3 + abs(N-2))
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in range(3, N + 1):
+        dp[i] = (dp[i-2] + dp[i-1]) % 15746
+
+    return dp[N] % 15746
+
+
+N = int(input())
+print(fibo(N))
