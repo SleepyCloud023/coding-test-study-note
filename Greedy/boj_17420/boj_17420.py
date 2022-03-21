8bf1e5cd1919a282115d252da1224c7c5e3e112a
SleepyCloud023 <gnflwhf352@gmail.com>
2022-03-21 17:14:12 +0900

    solved: 깊콘이 넘쳐흘러 재시도

--- Greedy/boj_17420/boj_17420.py
+++ Greedy/boj_17420/boj_17420.py
@@ -3,37 +3,52 @@
 
 
 import heapq
+import math
 import sys
 input = sys.stdin.readline
+
+
+class Ticket:
+    def __init__(self, d_day, left_day) -> None:
+        self.d_day = d_day
+        self.left_day = left_day
+
+    def __lt__(self, other):
+        if self.d_day < other.d_day:
+            return True
+        elif self.d_day == other.d_day:
+            return self.left_day >= other.left_day
+        else:
+            return False
 
 
 def solution(N, left_days: list, d_days: list):
     count = 0
     min_heap = []
-    ticket_of_day = dict()
+    min_left = 0
 
     for i in range(N):
         left_day = left_days[i]
         d_day = d_days[i]
-        heapq.heappush(min_heap, (left_day, d_day))
-        ticket_of_day[d_day] = ticket_of_day.get(d_day, 0) + 1
+        heapq.heappush(min_heap, Ticket(d_day, left_day))
 
-    for d_day in sorted(ticket_of_day.keys()):
-        num_d_day = ticket_of_day[d_day]
-        while(num_d_day > 0):
-            left, d = heapq.heappop(min_heap)
-            if d != d_day:
-                add_count = max(d - left, 0) // 30 + 1
-                count += add_count
-                left += 30 * add_count
-                heapq.heappush(min_heap, (left, d))
-            elif d == d_day:
-                if left < d:
-                    count += 1
-                    left += 30
-                    heapq.heappush(min_heap, (left, d_day))
-                else:
-                    num_d_day -= 1
+    while(min_heap):
+        today = min_heap[0].d_day
+        ticket_list = []
+
+        while(min_heap and min_heap[0].d_day == today):
+            ticket = heapq.heappop(min_heap)
+            ticket_list.append(ticket)
+
+        for ticket in ticket_list:
+            bigger_day = max(ticket.d_day, min_left)
+
+            if ticket.left_day < bigger_day:
+                Q = math.ceil((bigger_day - ticket.left_day) / 30)
+                ticket.left_day += Q * 30
+                count += Q
+
+        min_left = max([ticket.left_day for ticket in ticket_list])
 
     return count
 
@@ -44,3 +59,11 @@ if __name__ == '__main__':
     d_days = list(map(int, input().split()))
     result = solution(N, left_days, d_days)
     print(result)
+
+test_case = '''
+4
+24 2 3 29
+25 30 30 30
+
+=> 6
+'''
